338862b43f2b19018fc3526c2fe3ca21c9f57171
seanroughton <seanroughton@gmail.com>
2021-03-03 16:23:33 +0000

    test 2

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [responseAsText, setResponseAsText] = useState('');
+  const getNav = async () => {
+    const navUrl =
+      'http://yc-api.yoursclothing.com/api/navigation/v2/get?Token=9ba51c12-a444-458b-8a4c-d8ac6ffadfca&SiteName=Long Tall Sally&Language=ENG&NavigationName=Main-RNApp';
+    const response = await fetch(navUrl).then((response) => response.json());
+    setResponseAsText(JSON.stringify(response).toString());
+  };
+  getNav();
+
+  console.log(responseAsText);
+  if (responseAsText) return <View style={styles.container}>{<Text>{responseAsText}</Text>}</View>;
+  return <Text>Loading ..........</Text>;
 }
 
 const styles = StyleSheet.create({

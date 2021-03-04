8db95cc4b3375c7aa404e58723ed3595afce85d2
seanroughton <seanroughton@gmail.com>
2021-03-04 10:24:05 +0000

    error boundary

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ErrorBoundary from 'react-native-error-boundary';
 
-export default function App() {
+const TestFetch = () => {
+  const status = (response) => {
+    if (response.ok) return response;
+    var error = new Error(response.statusText || response.status);
+    error.response = response;
+    throw error;
+  };
+
   const [responseAsText, setResponseAsText] = useState('');
   const getNav = async () => {
     const navUrl =
       'http://yc-api.yoursclothing.com/api/navigation/v2/get?Token=9ba51c12-a444-458b-8a4c-d8ac6ffadfca&SiteName=Long Tall Sally&Language=ENG&NavigationName=Main-RNApp';
-    const response = await fetch(navUrl).then((response) => response.json());
+    const response = await fetch(navUrl)
+      .then(status)
+      .then((response) => response.json());
     setResponseAsText(JSON.stringify(response).toString());
   };
   getNav();
@@ -15,6 +25,14 @@ export default function App() {
   console.log(responseAsText);
   if (responseAsText) return <View style={styles.container}>{<Text>{responseAsText}</Text>}</View>;
   return <Text>Loading ..........</Text>;
+};
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <TestFetch />
+    </ErrorBoundary>
+  );
 }
 
 const styles = StyleSheet.create({
